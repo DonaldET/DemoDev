e5d468e3669d101c886bc8bc64714f7474c05943
Donald Trummell <dtrummell@gmail.com>
2014-08-04 14:21:52 -0700

    Standardize docs generation
    Add script to append doc files; use identical Eclipse formatting for
    DemoDev and WPE

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ##Project Intent
 
-These small _DemoDev_ projects illustrate useful techniques for building Java and J2EE related applications.  They use Maven to build the project artifacts and JUnit to test artifacts.  The _uber_ parent project defines common dependencies and versions. Child projects are located under the parent project, and individual plugins and _localized_ dependencies are included in child projects.
+These small _DemoDev_ projects illustrate useful techniques for building Java and J2EE related applications.  They use Maven to build the project artifacts and JUnit to test artifacts.  The _root_ parent project defines common MAVEN dependencies and versions. Nested child projects are located under the parent project, and individual plugins and _localized_ dependencies are included in child projects.  A child project may be a parent of lower level child projects forming a tree.
+
+_Source Documentation_: [The README.md file](http://donaldet.github.io/DemoDev/)
+
 
 ##Sub-Project Categories
 
---
+++ StaticResources/appendDocs.cmd
@@ -0,0 +1,80 @@
+::
+: appendDocs.cmd - 2014.8.4
+:
+: Assemble README.md from docs sub-directory
+::
+
+@echo off
+setlocal
+set XX_CurDir=%CD%
+echo.
+echo. **** Assembling README.md
+
+:
+:xx_setup_base
+set XX_pr1=
+call :XX_GetPath %0
+if not defined XX_pr1 goto XX_badPath
+pushd %XX_pr1%
+if ERRORLEVEL 1 goto XX_badPath
+goto xx_setup_files
+
+:xx_badPath
+echo.
+echo. -- Failed to determine path "%XX_pr1%"
+set XX_pr1=
+goto finis
+
+:
+:xx_setup_files
+set XX_srcDir=..\docs\
+set XX_masterFile=README.md
+set XX_targetDir=..\
+set XX_allDocs=%XX_targetDir%%XX_masterFile%
+
+if exist %XX_allDocs% del /F/Q %XX_allDocs%
+copy /a /y nul %XX_allDocs% > nul
+
+echo.   Append all files from %XX_srcDir%*.md into %XX_allDocs%
+for %%f in (%XX_srcDir%*.md) do (
+type %%f >> %XX_allDocs%
+)
+goto finis
+
+:
+:finis
+echo. **** Assembling README.md completed
+if defined XX_CurDir popd %XX_CurDir%
+
+set XX_CurDir=
+set XX_pr1=
+set XX_srcDir=
+set XX_masterFile=
+set XX_targetDir=
+set XX_allDocs=
+endlocal
+goto Done
+
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+::
+: Determine directory path
+::
+:XX_GetPath
+set XX_pr1=
+for %%I in (%1) do set XX_pr1=%%~dpI
+goto :EOF
+
+
+
+
+
+:
+:Done
+pause
+
+
+
+::
+: end of appendDocs.cmd
+::
