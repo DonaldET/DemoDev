452f14f6ab4e70f2e81342cd8660def04c2ef433
Donald E. Trummell <dtrummell@gmail.com>
2018-10-12 17:11:53 -0700

    Fix git-ignore
    ALso reformat test files

--- dev-topics-utils/dev-topics-generationutils/src/test/java/don/demo/generator/test/model/TestPropertyModelBuilder.java
+++ dev-topics-utils/dev-topics-generationutils/src/test/java/don/demo/generator/test/model/TestPropertyModelBuilder.java
@@ -25,130 +25,118 @@ import don.demo.generator.model.PropertyModelLoader;
  *         paragraph, appear in all copies, modifications, and distributions.
  *         Contact dtrummell@gmail.com for commercial licensing opportunities.
  */
-public class TestPropertyModelBuilder
-{
+public class TestPropertyModelBuilder {
+	private static final String SPRING_TEST_APP_CONTEXT = "/META-INF/main/spring/test-app-context.xml";
 
-    private static final String SPRING_TEST_APP_CONTEXT = "/META-INF/main/spring/test-app-context.xml";
+	private static boolean[] initialized = new boolean[] { false };
 
-    private static boolean[] initialized = new boolean[]
-    { false };
+	private static ApplicationContext ctx = null;
 
-    private static ApplicationContext ctx = null;
+	private PropertyModelBuilder modelBuilder = null;
+	private PropertyModelLoader modelLoader = null;
 
-    private PropertyModelBuilder modelBuilder = null;
-    private PropertyModelLoader modelLoader = null;
+	@Before
+	public void setUp() throws Exception {
+		synchronized (initialized) {
+			if (!initialized[0]) {
+				//
+				// Initialize
+				final ApplicationContext ctxT = new ClassPathXmlApplicationContext(SPRING_TEST_APP_CONTEXT);
+				Assert.assertNotNull(SPRING_TEST_APP_CONTEXT + " null", ctxT);
 
-    @Before
-    public void setUp() throws Exception
-    {
-        synchronized (initialized)
-        {
-            if (!initialized[0])
-            {
-                //
-                // Initialize
-                final ApplicationContext ctxT = new ClassPathXmlApplicationContext(SPRING_TEST_APP_CONTEXT);
-                Assert.assertNotNull(SPRING_TEST_APP_CONTEXT + " null", ctxT);
+				final String ver = (String) ctxT.getBean("version.test");
+				Assert.assertNotNull(SPRING_TEST_APP_CONTEXT + " null", ver);
 
-                final String ver = (String) ctxT.getBean("version.test");
-                Assert.assertNotNull(SPRING_TEST_APP_CONTEXT + " null", ver);
+				ctx = ctxT;
 
-                ctx = ctxT;
+				initialized[0] = true;
+			}
+		}
 
-                initialized[0] = true;
-            }
-        }
+		modelBuilder = (PropertyModelBuilder) ctx.getBean("modelbuilder");
+		modelLoader = (PropertyModelLoader) ctx.getBean("modelloader");
+	}
 
-        modelBuilder = (PropertyModelBuilder) ctx.getBean("modelbuilder");
-        modelLoader = (PropertyModelLoader) ctx.getBean("modelloader");
-    }
+	private String version = null;
+	private String testVersion = null;
 
-    private String version = null;
-    private String testVersion = null;
+	@After
+	public void tearDown() throws Exception {
+		modelBuilder = null;
+		modelLoader = null;
+	}
 
-    @After
-    public void tearDown() throws Exception
-    {
-        modelBuilder = null;
-        modelLoader = null;
-    }
+	@Test
+	public void testAllNeededBeansPresent() {
+		String name = "version";
+		version = (String) ctx.getBean(name);
+		Assert.assertEquals("bean " + name + " differs", "1.0.0-SNAPSHOT", version);
 
-    @Test
-    public void testAllNeededBeansPresent()
-    {
-        String name = "version";
-        version = (String) ctx.getBean(name);
-        Assert.assertEquals("bean " + name + " differs", "1.0.0-SNAPSHOT", version);
+		name = "version.test";
+		testVersion = (String) ctx.getBean(name);
+		Assert.assertEquals("bean " + name + " differs", "TEST-1.0.0-SNAPSHOT", testVersion);
 
-        name = "version.test";
-        testVersion = (String) ctx.getBean(name);
-        Assert.assertEquals("bean " + name + " differs", "TEST-1.0.0-SNAPSHOT", testVersion);
+		Assert.assertNotNull("modelLoader null", modelLoader);
+	}
 
-        Assert.assertNotNull("modelLoader null", modelLoader);
-    }
+	@Test
+	public void testEmptyPropertiesBuilder() {
+		modelBuilder.clear();
+		Assert.assertEquals("new builder not empty", 0, modelBuilder.build().size());
+	}
 
-    @Test
-    public void testEmptyPropertiesBuilder()
-    {
-        modelBuilder.clear();
-        Assert.assertEquals("new builder not empty", 0, modelBuilder.build().size());
-    }
+	@Test
+	public void testModelAugment() {
+		modelBuilder.clear();
+		Assert.assertEquals("new builder not empty", 0, modelBuilder.build().size());
+		final Properties aug = new Properties();
+		aug.setProperty("new key", "new value");
+		final PropertyModelBuilder augmented = modelBuilder.augment(aug);
+		Assert.assertTrue("simple augment failed", aug.equals(augmented.build()));
+	}
 
-    @Test
-    public void testModelAugment()
-    {
-        modelBuilder.clear();
-        Assert.assertEquals("new builder not empty", 0, modelBuilder.build().size());
-        final Properties aug = new Properties();
-        aug.setProperty("new key", "new value");
-        final PropertyModelBuilder augmented = modelBuilder.augment(aug);
-        Assert.assertTrue("simple augment failed", aug.equals(augmented.build()));
-    }
+	@Test
+	public void testModelAugmentOverride() {
+		modelBuilder.clear();
+		Assert.assertEquals("new builder not empty", 0, modelBuilder.build().size());
+		final Properties aug = new Properties();
+		aug.setProperty("new key", "new value");
+		aug.setProperty("override key", "old value");
+		Assert.assertEquals("simple augment failed", aug, modelBuilder.augment(aug).build());
 
-    @Test
-    public void testModelAugmentOverride()
-    {
-        modelBuilder.clear();
-        Assert.assertEquals("new builder not empty", 0, modelBuilder.build().size());
-        final Properties aug = new Properties();
-        aug.setProperty("new key", "new value");
-        aug.setProperty("override key", "old value");
-        Assert.assertEquals("simple augment failed", aug, modelBuilder.augment(aug).build());
+		final Properties augOvr = new Properties();
+		augOvr.setProperty("override key", "new value");
+		augOvr.setProperty("added key", "added value");
 
-        final Properties augOvr = new Properties();
-        augOvr.setProperty("override key", "new value");
-        augOvr.setProperty("added key", "added value");
+		final Properties augExp = new Properties();
+		augExp.setProperty("new key", "new value");
+		augExp.setProperty("override key", "new value");
+		augExp.setProperty("added key", "added value");
+		Assert.assertEquals("override augment failed", augExp, modelBuilder.augment(augOvr).build());
+	}
 
-        final Properties augExp = new Properties();
-        augExp.setProperty("new key", "new value");
-        augExp.setProperty("override key", "new value");
-        augExp.setProperty("added key", "added value");
-        Assert.assertEquals("override augment failed", augExp, modelBuilder.augment(augOvr).build());
-    }
+	/**
+	 * An integration test combining reading and sequentially augmenting models
+	 */
+	@Test
+	public void testLoadFlatPropertiesFile() {
+		final String testSpec = "classpath:model_read_tests/MyTestAppModel.properties";
+		final Properties model = modelLoader.loadProperties(testSpec);
+		Assert.assertNotNull("model" + testSpec + " null", model);
 
-    /**
-     * An integration test combining reading and sequentially augmenting models
-     */
-    @Test
-    public void testLoadFlatPropertiesFile()
-    {
-        final String testSpec = "classpath:model_read_tests/MyTestAppModel.properties";
-        final Properties model = modelLoader.loadProperties(testSpec);
-        Assert.assertNotNull("model" + testSpec + " null", model);
+		final String testSpecOver = "classpath:model_read_tests/MyTestAppOverride.properties";
+		final Properties modelOver = modelLoader.loadProperties(testSpecOver);
+		Assert.assertNotNull("model" + testSpecOver + " null", modelOver);
 
-        final String testSpecOver = "classpath:model_read_tests/MyTestAppOverride.properties";
-        final Properties modelOver = modelLoader.loadProperties(testSpecOver);
-        Assert.assertNotNull("model" + testSpecOver + " null", modelOver);
+		modelBuilder.clear();
+		final Properties finished = modelBuilder.augment(model).augment(modelOver).build();
 
-        modelBuilder.clear();
-        final Properties finished = modelBuilder.augment(model).augment(modelOver).build();
-
-        final Properties modelExp = new Properties();
-        modelExp.setProperty("app_title", "My Glorious Application");
-        modelExp.setProperty("an_object_id", "1250 Plymouth Ave.");
-        modelExp.setProperty("an_object_value", "Balboa Park Station");
-        modelExp.setProperty("override_me", "overridden");
-        modelExp.setProperty("override_specific", "by_override");
-        Assert.assertEquals("file based override augment failed", modelExp, finished);
-    }
+		final Properties modelExp = new Properties();
+		modelExp.setProperty("app_title", "My Glorious Application");
+		modelExp.setProperty("an_object_id", "1250 Plymouth Ave.");
+		modelExp.setProperty("an_object_value", "Balboa Park Station");
+		modelExp.setProperty("override_me", "overridden");
+		modelExp.setProperty("override_specific", "by_override");
+		Assert.assertEquals("file based override augment failed", modelExp, finished);
+	}
 }
--- dev-topics-utils/dev-topics-generationutils/src/test/java/don/demo/generator/test/model/TestPropertyModelLoader.java
+++ dev-topics-utils/dev-topics-generationutils/src/test/java/don/demo/generator/test/model/TestPropertyModelLoader.java
@@ -24,103 +24,90 @@ import don.demo.generator.model.PropertyModelLoader;
  *         paragraph, appear in all copies, modifications, and distributions.
  *         Contact dtrummell@gmail.com for commercial licensing opportunities.
  */
-public class TestPropertyModelLoader
-{
-    private static final String SPRING_TEST_APP_CONTEXT = "/META-INF/main/spring/test-app-context.xml";
+public class TestPropertyModelLoader {
+	private static final String SPRING_TEST_APP_CONTEXT = "/META-INF/main/spring/test-app-context.xml";
 
-    private static boolean[] initialized = new boolean[]
-    { false };
+	private static boolean[] initialized = new boolean[] { false };
 
-    private static ApplicationContext ctx = null;
+	private static ApplicationContext ctx = null;
 
-    private PropertyModelLoader modelLoader = null;
+	private PropertyModelLoader modelLoader = null;
 
-    @Before
-    public void setUp() throws Exception
-    {
-        synchronized (initialized)
-        {
-            if (!initialized[0])
-            {
-                //
-                // Initialize
-                final ApplicationContext ctxT = new ClassPathXmlApplicationContext(SPRING_TEST_APP_CONTEXT);
-                Assert.assertNotNull(SPRING_TEST_APP_CONTEXT + " null", ctxT);
+	@Before
+	public void setUp() throws Exception {
+		synchronized (initialized) {
+			if (!initialized[0]) {
+				//
+				// Initialize
+				final ApplicationContext ctxT = new ClassPathXmlApplicationContext(SPRING_TEST_APP_CONTEXT);
+				Assert.assertNotNull(SPRING_TEST_APP_CONTEXT + " null", ctxT);
 
-                final String ver = (String) ctxT.getBean("version.test");
-                Assert.assertNotNull(SPRING_TEST_APP_CONTEXT + " null", ver);
+				final String ver = (String) ctxT.getBean("version.test");
+				Assert.assertNotNull(SPRING_TEST_APP_CONTEXT + " null", ver);
 
-                ctx = ctxT;
+				ctx = ctxT;
 
-                initialized[0] = true;
-            }
-        }
+				initialized[0] = true;
+			}
+		}
 
-        modelLoader = (PropertyModelLoader) ctx.getBean("modelloader");
-    }
+		modelLoader = (PropertyModelLoader) ctx.getBean("modelloader");
+	}
 
-    private String version = null;
-    private String testVersion = null;
+	private String version = null;
+	private String testVersion = null;
 
-    @After
-    public void tearDown() throws Exception
-    {
-        modelLoader = null;
-    }
+	@After
+	public void tearDown() throws Exception {
+		modelLoader = null;
+	}
 
-    @Test
-    public void testAllNeededBeansPresent()
-    {
-        String name = "version";
-        version = (String) ctx.getBean(name);
-        Assert.assertEquals("bean " + name + " differs", "1.0.0-SNAPSHOT", version);
+	@Test
+	public void testAllNeededBeansPresent() {
+		String name = "version";
+		version = (String) ctx.getBean(name);
+		Assert.assertEquals("bean " + name + " differs", "1.0.0-SNAPSHOT", version);
 
-        name = "version.test";
-        testVersion = (String) ctx.getBean(name);
-        Assert.assertEquals("bean " + name + " differs", "TEST-1.0.0-SNAPSHOT", testVersion);
+		name = "version.test";
+		testVersion = (String) ctx.getBean(name);
+		Assert.assertEquals("bean " + name + " differs", "TEST-1.0.0-SNAPSHOT", testVersion);
 
-        Assert.assertNotNull("modelLoader null", modelLoader);
-    }
+		Assert.assertNotNull("modelLoader null", modelLoader);
+	}
 
-    @Test
-    public void testLoadFlatPropertiesFile()
-    {
-        final String testSpec = "classpath:model_read_tests/MyTestAppModel.properties";
-        final Properties model = modelLoader.loadProperties(testSpec);
-        Assert.assertNotNull("model" + testSpec + " null", model);
+	@Test
+	public void testLoadFlatPropertiesFile() {
+		final String testSpec = "classpath:model_read_tests/MyTestAppModel.properties";
+		final Properties model = modelLoader.loadProperties(testSpec);
+		Assert.assertNotNull("model" + testSpec + " null", model);
 
-        final Properties expected = new Properties();
-        expected.setProperty("app_title", "My Glorious Application");
-        expected.setProperty("an_object_id", "1250 Plymouth Ave.");
-        expected.setProperty("an_object_value", "Balboa Park Station");
-        expected.setProperty("override_me", "basic");
-        Assert.assertEquals("properties lengths differ", expected.size(), model.size());
+		final Properties expected = new Properties();
+		expected.setProperty("app_title", "My Glorious Application");
+		expected.setProperty("an_object_id", "1250 Plymouth Ave.");
+		expected.setProperty("an_object_value", "Balboa Park Station");
+		expected.setProperty("override_me", "basic");
+		Assert.assertEquals("properties lengths differ", expected.size(), model.size());
 
-        int n = 0;
-        int m = 0;
-        for (final Object key : model.keySet())
-        {
-            final Object ve = expected.get(key);
-            final Object vm = model.get(key);
-            boolean ne = ve == null;
-            if (ne)
-            {
-                ne = vm == null;
-            }
-            else
-            {
-                ne = ve.equals(vm);
-            }
+		int n = 0;
+		int m = 0;
+		for (final Object key : model.keySet()) {
+			final Object ve = expected.get(key);
+			final Object vm = model.get(key);
+			boolean ne = ve == null;
+			if (ne) {
+				ne = vm == null;
+			} else {
+				ne = ve.equals(vm);
+			}
 
-            if (!ne)
-            {
-                System.err.println("key[" + m + "] " + key + " differs; expected: " + ve + ";  model: " + vm);
-                n++;
-            }
+			if (!ne) {
+				System.err.println("key[" + m + "] " + key + " differs; expected: " + ve + ";  model: " + vm);
+				n++;
+			}
 
-            m++;
-        }
-        Assert.assertEquals("model property entries differ from expected", 0, n);
-        Assert.assertEquals("model inconsistency", model.size(), m);
-    }
+			m++;
+		}
+		Assert.assertEquals("model property entries differ from expected", 0, n);
+		Assert.assertEquals("model inconsistency", model.size(), m);
+	}
 }
