cd92fa7ad77a85f5a7d84c6faa8bcb1cd49b44d7
Donald E. Trummell <dtrummell@gmail.com>
2019-07-02 11:38:36 -0700

    Add batch script to run test
    Invoke locally in Eclipse environment.

---
+++ dev-topics-codingexams/dev-topics-liveramp-bitsearch/analysis/run_perform.cmd
@@ -0,0 +1,25 @@
+::::
+: run_perform.cmd
+:
+: Execute a performance run to test BitSearcher; ordered vs unordered search
+::::
+@echo off
+echo.
+echo. Execute PerformanceRunn to test BitSearcher; ordered vs unordered search
+echo.
+setlocal
+set path2classes=..\target\classes
+java -cp .;%path2classes% demo.liveramp.bitsearch.performance.PerformanceRunner
+if ERRORLEVEL 1 goto error
+goto finis
+:
+:error
+echo.
+echo. Performance run failed!
+echo.
+endlocal
+:
+:finis
+echo.
+echo. Done
+echo.
