b6ff172ee1191f5de5bfc2b5f3073b4a12d0257e
Donald E. Trummell <dtrummell@gmail.com>
2019-09-24 11:39:19 -0700

    Final publication; add copyright.

--- dev-topics-algorithms/dev-topics-nasa-sensor/src/main/java/demo/algo/sensor/SensorMonitoring.java
+++ dev-topics-algorithms/dev-topics-nasa-sensor/src/main/java/demo/algo/sensor/SensorMonitoring.java
@@ -4,7 +4,18 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
+/* 
+ * Copyright (c) 2019. Donald E. Trummell. All Rights Reserved.
+ * Permission to use, copy, modify, and distribute this software and its documentation
+ * for educational, research, and not-for-profit purposes, without fee and without
+ * a signed licensing agreement, is hereby granted, provided that the above
+ * copyright notice, and this paragraph, appear in all copies, modifications, and
+ * distributions. Contact dtrummell@gmail.com for commercial licensing opportunities.
+ */
+
 /**
+ * Shared class, constant, problem definitions.
+ * <p>
  * <strong>Problem Definition</strong>
  * <p>
  * We have a square radiation sensor with an overlaid grid of exposure regions,
