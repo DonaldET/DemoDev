4f643f03c60c6d5cc9fec385641bde023f0c5986
Donald E. Trummell <dtrummell@gmail.com>
2019-11-26 10:34:36 -0800

    Complete project maven plugin scanner
    Look in pom.xml files and find all "maven-" plugins.

--- StaticResources/helpers/src/unittest/resources/findPlugins.cmd
+++ StaticResources/helpers/src/unittest/resources/findPlugins.cmd
@@ -1,22 +1,26 @@
 ::
 : findPlugins.cmd
 :
-: Locates plugin entries in nested pom.xml files. Note: perform a maven clean prior to running
+: Locates plugin entries in nested pom.xml files below the base project directory. Notes:
+: o Uses relative addressing to find maven project root (e.g.,
+: \GitHub\DemoDev\StaticResources\helpers\src\unittest\resources\..\..\..\..\..)
+: o perform a maven clean prior to running
 ::
 @echo off
 setlocal
 set proj=..\..\..\..\..
-echo. Capturing tokens in pom.xml files below %proj%
+set scriptDir=xxx
+for %%I in (%0) do set scriptDir=%%~dpI
+set baseDir=%scriptDir%%proj%
+echo. Capturing tokens in pom.xml files below %baseDir%
 echo.
 
-pushd %proj%
+pushd %baseDir%
 findstr /S /C:"<artifactId>maven-" pom.xml
 popd
 goto finis
 
-
-
 :finis
 echo.
-echo. Captured tokens in pom.xml below %proj%
+echo. Captured tokens in pom.xml below %baseDir%
 endlocal
--- StaticResources/structure_info/get_pom_tokens.cmd
+++ StaticResources/structure_info/get_pom_tokens.cmd
@@ -1,12 +1,17 @@
+::
+: get_pom_tokens.cmd
+:
+: Locates maven plugin entries in nested pom.xml files defined by the script findPlugins.cmd.
+: Note: run mvn clean prior to scan.
+::
 @echo off
 setlocal
 set lst=maven-plugins.txt
+set finder=..\helpers\src\unittest\resources
 echo.
-echo. Listing MAVEN plugins in POM files into file %lst%
+echo. Listing MAVEN plugins in POM files into file %lst% using %finder%
 echo. Execute maven clean first!
-pushd ..\helpers\src\unittest\resources\
-call findPlugins.cmd > %lst%
-
+call %finder%\findPlugins.cmd > %lst%
 echo. Done with %lst%
 endlocal
 
