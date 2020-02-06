73b48c2aa60bcf8f00bec5866c768a221593f384
Donald E. Trummell <dtrummell@gmail.com>
2020-02-06 12:07:02 -0800

    Complete package chnange.

---
+++ dev-topics-algorithms/dev-topics-nasa-sensor/src/test/java/don/demo/algo/sensor/test/TestExposureHybridPS.java
@@ -0,0 +1,34 @@
+package don.demo.algo.sensor.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import don.demo.algo.sensor.ExposureAreaFinder;
+import don.demo.algo.sensor.MonitorExposureHybridPS;
+
+/**
+ * Apply bursts and compute area, compare to expected.
+ *
+ * @author Donald Trummell (dtrummell@gmail.com)
+ */
+public class TestExposureHybridPS {
+	private ExposureAreaFinder finder = null;
+
+	@Before
+	public void setUp() throws Exception {
+		finder = new MonitorExposureHybridPS();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		finder = null;
+	}
+
+	@Test
+	public void testCases() {
+		for (int id = 0; id < GenerateDataAndTest.testCases; id++) {
+			GenerateDataAndTest.testData(id, finder, false);
+		}
+	}
+}
