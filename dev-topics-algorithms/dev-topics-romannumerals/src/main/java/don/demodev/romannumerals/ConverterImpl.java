400d7ff96207453c683fc54dd15b06b329f2c093
Donald E. Trummell <dtrummell@gmail.com>
2020-08-08 19:16:20 -0700

    Complete initial test setup
    Add a unit test for empty string, generate initial methods.

--- dev-topics-algorithms/dev-topics-romannumerals/src/main/java/don/demodev/romannumerals/ConverterImpl.java
+++ dev-topics-algorithms/dev-topics-romannumerals/src/main/java/don/demodev/romannumerals/ConverterImpl.java
@@ -2,8 +2,9 @@ package don.demodev.romannumerals;
 
 /**
  * Convert a binary integer to Roman numerals; convert a string of Roman
- * numerals to binary.. We generally print binary numbers using Arabic numerals.
- * This <a href= "https://en.wikipedia.org/wiki/Roman_numerals">Wikipedia Roman
+ * numerals to binary. We generally print integers (binary numbers) using Arabic
+ * numerals. This
+ * <a href= "https://en.wikipedia.org/wiki/Roman_numerals">Wikipedia Roman
  * Numerals</a> reference describes Roman numerals.
  * <p>
  * The problem of Roman numerals representing numbers is similar the classical
@@ -18,7 +19,8 @@ public class ConverterImpl implements Converter {
 	public ConverterImpl() {
 	}
 
-	public String arabic2Roman(final int arabic) {
+	@Override
+	public String arabic2Roman(int arabic) {
 		if (arabic < 0) {
 			throw new IllegalArgumentException("argument small");
 		}
@@ -26,14 +28,15 @@ public class ConverterImpl implements Converter {
 			throw new IllegalArgumentException("argument large");
 		}
 
-		return convertRoman2ArabicImpl(0, arabic);
+		return convertRoman2ArabicImpl(arabic);
 	}
 
-	private String convertRoman2ArabicImpl(final int idx, final int num) {
-		return null;
-	}
-
-	public int roman2Arabic(final String roman) {
+	@Override
+	public int roman2Arabic(String roman) {
 		throw new IllegalStateException("no implemented");
+	}
+
+	private String convertRoman2ArabicImpl(final int arabic) {
+		return "";
 	}
 }
--- dev-topics-algorithms/dev-topics-romannumerals/src/test/java/don/demodev/romannumerals/test/Arabic2RomanTest.java
+++ dev-topics-algorithms/dev-topics-romannumerals/src/test/java/don/demodev/romannumerals/test/Arabic2RomanTest.java
@@ -33,6 +33,8 @@ public class Arabic2RomanTest {
 		Assert.assertEquals("small param", "", converter.arabic2Roman(0));
 	}
 
+	// Initial test sequence
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testNegativeParameterFails() {
 		Assert.assertEquals("negative param fails", "BAD", converter.arabic2Roman(-1));
@@ -40,6 +42,11 @@ public class Arabic2RomanTest {
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testBigParameterFails() {
-		Assert.assertEquals("Big param fails", "BAD", converter.arabic2Roman(4000));
+		Assert.assertEquals("Big param fails", "BAD", converter.arabic2Roman(4001));
+	}
+
+	@Test
+	public void testInitial0() {
+		Assert.assertEquals("0 failed", "", converter.arabic2Roman(0));
 	}
 }
