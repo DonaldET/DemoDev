6e86bad18ed8b35a03b2864faa0a41c2758a531c
Donald E. Trummell <dtrummell@gmail.com>
2022-06-14 22:17:32 -0700

    Clean up batch test run command.
    Capture compete output and timing.

--- dev-topics-dataengineering/dev-topics-concurrentget/run_all.cmd
+++ dev-topics-dataengineering/dev-topics-concurrentget/run_all.cmd
@@ -2,6 +2,8 @@
 :
 : Run the three test classes with timming
 :
+: Invocation: run_all.cmd 1> run_all.lst 2>&1
+:
 @echo off
 echo.
 echo. Run the 3 test classes for different levels of concurrency
@@ -9,16 +11,17 @@ echo.
 set xx_cp=target\classes\
 
 echo.
-echo. "SequentialRunner from %xx_cp% using %JAVA_OPTIONS%"
+echo. === SequentialRunner from %xx_cp% using %JAVA_OPTIONS%
 java %JAVA_OPTIONS% -cp .;%xx_cp% don.demo.concurrent.SequentialRunner
 if ERRORLEVEL 1 goto run1_failed
 
-echo. "ConcurrentRunner from %xx_cp% using %JAVA_OPTIONS%"
+echo.
+echo. === ConcurrentRunner from %xx_cp% using %JAVA_OPTIONS%
 java %JAVA_OPTIONS% -cp .;%xx_cp% don.demo.concurrent.ConcurrentRunner
 if ERRORLEVEL 1 goto run2_failed
 
 echo.
-echo. "HighlyConcurrentRunner from %xx_cp% using %JAVA_OPTIONS%"
+echo. === HighlyConcurrentRunner from %xx_cp% using %JAVA_OPTIONS%
 java %JAVA_OPTIONS% -cp .;%xx_cp% don.demo.concurrent.HighlyConcurrentRunner
 if ERRORLEVEL 1 goto run3_failed
 
