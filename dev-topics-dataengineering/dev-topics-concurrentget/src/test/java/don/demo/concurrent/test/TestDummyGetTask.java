5c34bd55b17cc4f47910d0e369a314a95ddb8a5b
Donald E. Trummell <dtrummell@gmail.com>
2022-06-15 14:23:32 -0700

    Correct unit test checksum calculations
    Changed dummy task so the values changed.

--- dev-topics-dataengineering/dev-topics-concurrentget/src/test/java/don/demo/concurrent/test/TestDummyGetTask.java
+++ dev-topics-dataengineering/dev-topics-concurrentget/src/test/java/don/demo/concurrent/test/TestDummyGetTask.java
@@ -38,27 +38,27 @@ public class TestDummyGetTask {
 		new DummyGetTask(processState, remotes.get(0)).accept(remotes.get(0).id());
 		Assert.assertEquals("after 0 count bad", 1, processState.taskCount.get());
 		Assert.assertEquals("after 0 failed count bad", 0, processState.failedTaskCount.get());
-		Assert.assertEquals("after 0 checksum bad", 563L, processState.checkSum.get());
+		Assert.assertEquals("after 0 checksum bad", 467771033L, processState.checkSum.get());
 		Assert.assertEquals("after 0 byteCount bad", 4L, processState.byteCount.get());
 
 		new DummyGetTask(processState, remotes.get(2)).accept(remotes.get(2).id());
 		Assert.assertEquals("after 0 count bad", 2, processState.taskCount.get());
 		Assert.assertEquals("after 0 failed count bad", 0, processState.failedTaskCount.get());
-		Assert.assertEquals("after 0 checksum bad", 1232L, processState.checkSum.get());
+		Assert.assertEquals("after 0 checksum bad", -457729379L, processState.checkSum.get());
 		Assert.assertEquals("after 0 byteCount bad", 8L, processState.byteCount.get());
 
 		new DummyGetTask(processState, remotes.get(1)).accept(remotes.get(1).id());
 		Assert.assertEquals("after 0 count bad", 3, processState.taskCount.get());
 		Assert.assertEquals("after 0 failed count bad", 1, processState.failedTaskCount.get());
-		Assert.assertEquals("after 0 checksum bad", 1232L, processState.checkSum.get());
+		Assert.assertEquals("after 0 checksum bad", -457729379L, processState.checkSum.get());
 		Assert.assertEquals("after 0 byteCount bad", 8L, processState.byteCount.get());
 	}
-	
+
 	@Test
 	public void testDelay() {
 		long start = System.nanoTime();
 		new DummyGetTask(processState, remotes.get(0)).accept(remotes.get(0).id());
-		 long elapsed = System.nanoTime() - start;
-		 Assert.assertTrue("wait failed, got " + elapsed , elapsed >= 1000000000);
+		long elapsed = System.nanoTime() - start;
+		Assert.assertTrue("wait failed, got " + elapsed, elapsed >= 1000000000);
 	}
 }
