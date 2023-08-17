edba4665425dfe5306571ba2c0a950f67dfb6dfa
Donald E. Trummell <dtrummell@gmail.com>
2023-08-16 23:12:32 -0700

    Add read created CSV to smoke test

--- dev-topics-bigdata/dev-topics-sparkinstall/examples/smoketest/smoketest.py
+++ dev-topics-bigdata/dev-topics-sparkinstall/examples/smoketest/smoketest.py
@@ -8,7 +8,17 @@ import time
 shared_file_name = '/tmp/spark_output/smoketest'
 
 
-def default_parallelism(session):
+def _get_spark_session():
+    session = (SparkSession.
+               builder.
+               master("local[*]").
+               appName('CollectedSmokeTests1').
+               getOrCreate())
+    session.sparkContext.setLogLevel('WARN')
+    return session
+
+
+def _default_parallelism(session):
     # based on https://stackoverflow.com/questions/51342460/getexecutormemorystatus-size-not-outputting-correct-num-of-executors?noredirect=1&lq=1
     time.sleep(15)
     dp_exc = session._jsc.sc().defaultParallelism()
@@ -17,9 +27,7 @@ def default_parallelism(session):
 
 def smoke1(name):
     print(f"\nSmoke Test 1: {name}")
-    spark = SparkSession.builder.master("local[*]") \
-        .appName('CollectedSmokeTests1') \
-        .getOrCreate()
+    spark = _get_spark_session()
 
     print('Python Version:' + spark.sparkContext.pythonVer)
     print('Spark Version :' + spark.version)
@@ -37,10 +45,8 @@ def smoke2(name):
     # Taken, in part, from:
     # https://github.com/spark-examples/pyspark-examples/blob/master/convert-column-python-list.py
     print(f"\nSmoke Test 2: {name}")
-    spark = SparkSession.builder.master("local[*]") \
-        .appName('CollectedSmokeTests2') \
-        .getOrCreate()
-    print('Default Parallelism: ' + str(default_parallelism(spark)))
+    spark = _get_spark_session()
+    print('Default Parallelism: ' + str(_default_parallelism(spark)))
 
     data = [("James", "Smith", "USA", "CA"), ("Michael", "Rose", "USA", "NY"),
             ("Robert", "Williams", "USA", "CA"), ("Maria", "Jones", "USA", "FL")]
@@ -70,12 +76,22 @@ def smoke2(name):
 
 
 def smoke3(name):
-    # Taken, in part, from:
     print(f"\nSmoke Test 3: {name}")
-    spark = SparkSession.builder.master("local[*]") \
-        .appName('CollectedSmokeTests3') \
-        .getOrCreate()
+    spark = _get_spark_session()
+
+    print("\n**************************************")
+    print("**** Read DataFrame as a CSV file ****")
+    print("**************************************")
+    print(f"  -- File: {shared_file_name}")
+    df = spark.read.options(header='True', inferSchema='True', delimiter=',').csv(shared_file_name)
+    print("*** DataFrame Read as a CSV file ***")
+    print("***************************************")
+    df.printSchema()
+    print('  -- Display')
+    df.show()
+
     spark.stop()
+    print('\nSmoke Test3 Done')
 
 
 if __name__ == '__main__':
@@ -83,8 +99,10 @@ if __name__ == '__main__':
     print('*** PySpark Smoke Tests ***')
     print('***************************')
 
-    smoke1('Spark Attributes')
+    print('\n-----------------------------------------------')
+    smoke1('Print Spark Attributes')
     smoke2('Create a DataFrame')
     smoke3('Read a DataFrame')
+    print('-----------------------------------------------')
 
     print('PySpark Smoke Tests Done.')
