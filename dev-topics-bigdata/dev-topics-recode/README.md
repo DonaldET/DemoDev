66cdc5f702ac5fa4facc0d8348af00614d6b84c3
Donald E. Trummell <dtrummell@gmail.com>
2023-08-18 14:06:18 -0700

    Initial Recode (native2ascii replacement) check-in

---
+++ dev-topics-bigdata/dev-topics-recode/README.md
@@ -0,0 +1,1 @@
+# don.demo.fileutil/Recode
---
+++ dev-topics-bigdata/dev-topics-recode/src/test/java/don/demo/fileutil/codeac/RecoderTest.java
@@ -0,0 +1,15 @@
+package don.demo.fileutil.codeac;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class RecoderTest {
+
+    @Test
+    void testIfCompanyWillSucceed() {
+        boolean goodIdea = true;
+        assertEquals(goodIdea, true);
+    }
+
+}
